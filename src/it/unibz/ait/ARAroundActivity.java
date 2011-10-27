606b568b3f2155f6eeb26f5e36dbd6416c32d9f1
Muslim Chochlov <muslim.chochlov@gmail.com>
2011-10-27 18:23:38 +0200

    Initial commit

---
+++ src/it/unibz/ait/ARAroundActivity.java
@@ -0,0 +1,13 @@
+package it.unibz.ait;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ARAroundActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

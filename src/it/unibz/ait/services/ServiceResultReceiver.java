f9eff97dc7d5ab9e7675538bcf6dcbdd4f08f790
Muslim Chochlov <muslim.chochlov@gmail.com>
2011-11-22 20:30:19 +0100

    Adding result receiver class

---
+++ src/it/unibz/ait/services/ServiceResultReceiver.java
@@ -0,0 +1,29 @@
+package it.unibz.ait.services;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
+
+public class ServiceResultReceiver extends ResultReceiver {
+
+	private Receiver mReceiver;
+
+    public ServiceResultReceiver(Handler handler) {
+        super(handler);
+    }
+
+    public void setReceiver(Receiver receiver) {
+        mReceiver = receiver;
+    }
+
+    public interface Receiver {
+        public void onReceiveResult(int resultCode, Bundle resultData);
+    }
+
+    @Override
+    protected void onReceiveResult(int resultCode, Bundle resultData) {
+        if (mReceiver != null) {
+            mReceiver.onReceiveResult(resultCode, resultData);
+        }
+    }
+}
